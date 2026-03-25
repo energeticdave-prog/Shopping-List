d691bc0e4971b78f3131a31400d2705852a536eb
energeticdave-prog <energeticdave@gmail.com>
2026-03-26 10:20:01 +1100

    Delete playwright.config.js

--- playwright.config.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-const { defineConfig } = require('@playwright/test');
-
-module.exports = defineConfig({
-  use: {
-    headless: true,
-    baseURL: 'https://shopping-list-liard-one.vercel.app',
-  },
-});
